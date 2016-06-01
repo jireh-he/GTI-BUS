84003534e8d36ce9b1854cad934e0ff15524908e
jireh-he <18772119@qq.com>
2016-06-01 17:47:06 +0800

    city poi

---
+++ doc/asset/js/bdpoi.js
@@ -0,0 +1,159 @@
+/**
+ * 2016-5-30
+ * By Jireh
+ * 
+ */
+var BMapExt,map,container,myChart;
+(function () {
+require.config({
+    paths: {
+
+    	echarts:'./doc/asset/js',
+    },
+    packages: [
+               {
+                   name: 'BMap',
+                   location: './doc/asset/js/BMap/src',
+                   main: 'main'
+               }
+           ]
+});
+var cityname='厦门';
+require(
+	    [
+	        'echarts',
+	        'BMap',
+	        'echarts/chart/map'
+	    ],
+	    function (echarts, BMapExtension) {
+
+	        // 初始化地图
+	        BMapExt = new BMapExtension($('#allmap')[0], BMap, echarts,{
+	            enableMapClick: true
+	        });
+	        map = BMapExt.getMap();
+	        container = BMapExt.getEchartsContainer();
+	        map.centerAndZoom(cityname,12);
+	        map.enableScrollWheelZoom(true);
+	    }
+	    );
+var legendItems=['公园','银行'];
+$("#legend-list").html(makeLegend(legendItems));
+$(".legend-item").on('click',function(){
+		$(this).find('.item-checkbox').toggle();
+		var checked=$(this).find('.item-checkbox').css('display');
+		if(checked=='block'){
+			cityPoi(cityname, $(this));
+		}
+});
+
+})();
+var cityPoi=function(cityname,legendItem){
+				var searchStr=legendItem.find('.item-title').text();
+				
+		        poiSearch(map, searchStr, 
+		        		function(g){
+		        	
+		        	if(myChart){
+		        		var option=myChart.getOption();
+		        		var series=option.series;
+		        		var exist=false;
+		        		var k=0;
+		        		for(k=0;k<series.length;k++){
+		        			if(series[k].name==searchStr)
+		        				break;
+		        		}
+	        			var newoption=makeOption(cityname+'POI展示', g, legendItem);
+	        			series[k]=newoption.series[0];
+		
+		        	}else{
+		        		myChart = BMapExt.initECharts(container);
+		        		var option=makeOption(cityname+'POI展示',g,legendItem);
+		        	}
+			        window.onresize = myChart.resize;
+			        
+			        BMapExt.setOption(option, true);
+		        		
+		        		});
+};
+var poiSearch=function(map,searchstr,optionCallback){
+	var s={};
+	var j=1;
+	var options = {
+			onSearchComplete: function(results){
+				// 判断状态是否正确
+				if (local.getStatus() == BMAP_STATUS_SUCCESS){
+					for (var i = 0; i < results.getCurrentNumPois(); i ++){
+						s[results.getPoi(i).title]=[results.getPoi(i).point.lng,results.getPoi(i).point.lat];
+						
+					}
+	                if(results.getPageIndex!=results.getNumPages())  
+	                {
+	                        local.gotoPage(j);
+	                        j=j+1;
+	                }   
+	                
+					optionCallback(s);
+				}
+			}
+		};
+		var local = new BMap.LocalSearch(map, options);
+		local.search(searchstr);
+};
+
+var makeOption=function(title,geolist,legendItem){
+	var searchStr=legendItem.find('.item-title').text();
+	var color=legendItem.find('.colorbox').css('background-color');
+	var d=[];
+    for(var key in geolist){
+    	d.push({name:key});
+    }
+    legendItem.find('.stats_count').text(d.length);
+	return option={
+		title:{
+			text:title,
+			x:'center'
+		},
+		tooltip:{
+			trigger:'item'
+		},
+		toolbox:{
+			show:true,
+			orient:'vertical',
+			x:'right',
+			y:'center',
+	        feature : {
+	            mark : {show: true},
+	            dataView : {show: true, readOnly: false},
+	            restore : {show: true},
+	            saveAsImage : {show: true}
+	        }
+		},
+		series:[
+		    {
+		    	name:searchStr,
+		    	type:'map',
+		    	mapType:'none',
+		    	data:[],
+		    	geoCoord:geolist,
+		    	markPoint:{
+		    		symbol:'emptyCircle',
+		            symbolSize : 3,
+	                effect : {
+	                    show: false,
+	                    shadowBlur : 0
+	                },
+	                itemStyle:{
+	                    normal:{
+	                        label:{show:false},
+	                        color:color,
+	                    }
+	                },
+	                data:d,
+		    	}
+		    }    
+		        ]
+		
+	};
+}
+
---
+++ doc/asset/js/poihome.js
@@ -0,0 +1,87 @@
+var activeClass = {};
+var loc = {};
+loc.img = './doc';
+$('#head')[0].innerHTML =
+    '<div class="container">'
+        + '<div class="navbar-header">'
+          + '<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">'
+            + '<span class="sr-only">Toggle navigation</span>'
+            + '<span class="icon-bar"></span>'
+            + '<span class="icon-bar"></span>'
+            + '<span class="icon-bar"></span>'
+          + '</button>'
+          + '<a class="navbar-brand" href="#">POI MAP</a>'
+        + '</div>'
+        + '<div class="navbar-collapse collapse" id="nav-wrap">'
+        + '<ul class="nav navbar-nav navbar-right" id="nav" style="max-width:100%;">'
+        +'<li class="dropdown ' + (activeClass.doc || activeClass.option || '') + '">'
+        + '<a href="#" class="dropdown-toggle" data-toggle="dropdown">导航<b class="caret"></b></a>'
+        + '<ul class="dropdown-menu">'
+          + '<li class=""><a href="#">地图</a></li>'
+        + '</ul>'
+      + '</li>'
+      + '</ul>'
+        + '</div><!--/.nav-collapse -->'
+        + '</div>';
+
+
+
+function back2Top() {
+    $("body,html").animate({scrollTop:0},1000);
+    return false;
+}
+
+
+var $footer = $('#footer');
+if ($footer.length) {
+    $footer[0].style.marginTop = '50px';
+    $footer[0].innerHTML =
+         '<div class="container">'
+            + '<div class="row" style="padding-bottom:20px;">'
+                + '<div class="col-md-3">'
+                    + '<p>GTI团队出品</p>'
+                    + '<ul>'
+                        + '<li><a href="#" target="_blank">GTI研究小组</a></li>'
+                    + '</ul>'
+                + '</div>'
+                + '<div class="col-md-3">'
+                    + '<p>更多</p>'
+                    + '<ul>'
+                        + '<li><a href="https://github.com/jireh-he/GTI-BUS/master/LICENSE.txt" target="_blank">License</a></li>'
+                    + '</ul>'
+                + '</div>'
+                + '<div class="col-md-3">'
+                    + '<p>联系我们</p>'
+                    + '<ul>'
+                        + '<li><a href="https://github.com/jireh-he/GTI-BUS" target="_blank"> Github</a></li>'
+                    + '</ul>'
+                + '</div>'
+                + '<div class="col-md-3 flogo">'
+                    + '<a href="javascript:void(0)" onclick="back2Top()" title="回到顶部"><img src="'+ (loc.img || '.')+ '/asset/img/echarts-logo2.png" alt="GTI"/></a>'
+                + '</div>'
+            + '</div>'
+            + '<p class="pull-right"><a href="javascript:void(0)" onclick="back2Top()" >Back to top</a></p>'
+            + '<p>&copy; 2016 <a href="#" target="_blank">Ato&Jireh</a></p>'
+        + '</div>';
+};
+var getRandomColor = function(){
+    return '#'+('00000'+(Math.random()*0x1000000<<0).toString(16)).substr(-6);
+  }
+function makeLegend(legendlist){
+	legendnum=legendlist.length;	
+	var legendhtml=''
+	for(var i=0;i<legendnum;i++){
+		legendhtml+='<div class="legend-item">'
+			+'<div class="colorbox" style="background-color:'
+			+getRandomColor()+';">'
+			+'<div class="item-checkbox">'
+			+'<span class="glyphicon glyphicon-ok">'
+			+'</span></div></div><div class="legend-item-text">'
+			+'<strong><span class="pull-right stats_count" id="stats_count_'+i+'">'
+			+'</span> </strong><span class="item-title">'
+			+legendlist[i]+'</span></div></div>';
+			
+	}
+	return legendhtml;
+}
+
